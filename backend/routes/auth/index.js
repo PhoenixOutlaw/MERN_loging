60b2a1842a0007cbfc4daab7835fae328da9072a
PhoenixOutlaw <harshupgrader@gmail.com>
2022-02-14 02:46:35 +0530

    bugs

--- backend/routes/auth/index.js
+++ backend/routes/auth/index.js
@@ -6,13 +6,10 @@ const userschema = require('../../models/userschema');
 const route = express.Router();
 
 
-route.get('/',(req, res) =>{
-  res.send("hdhfgd")
-})
 route.post('/login',(req, res) =>{
     const re = req.body;
    userschema.find({email:re.email},async (err, data) =>{
-     if(!data.length) res.send("account does not exits")
+     if(!data.length) res.send(false)
      else{
        if(await bcrypt.compare(re.password,data[0].password)){
          const token = jwt.sign({_id:data[0]._id},process.env.JWT_ACESS_TOKEN)
@@ -25,7 +22,6 @@ route.post('/login',(req, res) =>{
 
 route.post('/verify',(req, res) =>{
   const re = req.body;
-  console.log(re)
   jwt.verify(re.token,process.env.JWT_ACESS_TOKEN,(er,token)=>{
     if(er)res.send("forbidden")
     else {
